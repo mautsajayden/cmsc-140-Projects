886cda1bb3fc7a638ab241973c8ee87c34f48f00
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2024-05-23 01:30:27 -0400

    Add files via upload

---
+++ JMautsa_CW_[ 1].cpp
@@ -0,0 +1,24 @@
+/* Class: CMSC140 CRN
+* Instructor : Farnaz Eivazi
+	*Project<number> 1 
+	* Description : (Give a brief description for Project) Project displays user information such as name, major, class and their statement of choice 
+	* Due Date :September 7, 2023 
+*I pledge that I have completed the programming assignment independently.
+I have not copied the code from a student or any source.
+I have not given my code to any student.
+Print your Name here : _Jayden Mautsa _________
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "*******************" << endl;
+	cout << "Hello CMSC140 CRN 20433 class!" << endl;
+	cout << "My name is Jayden Mautsa." << endl;
+	cout << "My major is Computer Science." << endl;
+	cout << "Nice to see you" << endl;
+	cout << "Today is Semptember 5th" << endl;
+	cout << "*******************" << endl;
+	return 0;
+}
